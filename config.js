1085541489cda9baec6ea92ecb8831a2514d3da8
Ayush <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-05-12 15:12:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.owner = [
 ] //Number of owners
 
 global.mods = ['16468338933','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.prems = ['16468338933', '32460248586', '919398758484']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -46,7 +46,7 @@ global.APIKeys = { // APIKey Here
   'https://api-fgmods.ddns.net': 'fg-dylux'
 }
 
-// Sticker WM
+// Sticker WM// if you want to changr it
 global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
